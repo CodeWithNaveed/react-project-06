381790de8f0efd4e74ca187e092c9bc510616a50
CodeWithNaveed <getinfo.naveed@gmail.com>
2024-10-20 15:58:00 +0500

    'set'

--- src/Cards/Card/index.js
+++ src/Cards/Card/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function Card() {
@@ -7,65 +7,54 @@ function Card() {
             id: 1,
             question: "What language is React based on?",
             answer: "React is based on JavaScript.",
-            activeClass: "",
             value: null,
         },
         {
             id: 2,
             question: "What are the building blocks of React apps?",
             answer: "State, props, and components.",
-            activeClass: "",
             value: null,
         },
         {
             id: 3,
             question: "What's the name of the syntax we use to describe a UI in React?",
             answer: "The syntax used to describe a UI in React is called JSX (JavaScript XML)",
-            activeClass: "",
             value: null,
         },
         {
             id: 4,
             question: "How to pass data from parent to child components?",
             answer: "Data is passed from parent to child components in React using props.",
-            activeClass: "",
             value: null,
         },
         {
             id: 5,
             question: "How to give components memory?",
             answer: "To give components memory in React, you use state with the useState hook.",
-            activeClass: "",
             value: null,
         },
         {
             id: 6,
             question: "What do we call an input element that is completely synchronised with state?",
             answer: "An input element that is completely synchronized with state is called a controlled component.",
-            activeClass: "",
             value: null,
         }
     ]);
 
-
-    let num;
-    
     const handleClick = (index) => {
+        console.log(index);
         let newWord = [...word];
-        num = (newWord[index].id);
-        newWord[index].value = num;
-        setWord(newWord);
+        newWord[index].value = newWord[index].value === null ? newWord[index].id : null; // Toggle value
+        setWord(newWord); // Update state
     };
-
-
 
     return (
         word.map((v, i) => (
-            <h1 onClick={() => handleClick(i)}>
+            <h1 key={v.id} onClick={() => handleClick(i)} className={v.value === v.id ? "focus" : ""}>
                 {v.value === v.id ? v.answer : v.question}
             </h1>
         ))
     );
-};
+}
 
 export default Card;
